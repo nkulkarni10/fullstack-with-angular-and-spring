a35fdfcc9bbdda83f59b33fb0e2b80a497fc4011
Kulkarni <n.kulkarni@cgi.com>
2023-05-11 13:25:05 -0300

    Added logout functionality

---
+++ frontend/todo/src/app/logout/logout.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { HardcodedAuthenticationService } from '../service/hardcoded-authentication.service';
+
+@Component({
+  selector: 'app-logout',
+  templateUrl: './logout.component.html',
+  styleUrls: ['./logout.component.css']
+})
+export class LogoutComponent {
+
+  constructor(private hardcodedAuthenticationService: HardcodedAuthenticationService){
+    this.hardcodedAuthenticationService.logout();
+  }
+}
