bd7776d96fa28861be05981b1c55b09b15f9cadb
nkulkarni10 <74134288+nkulkarni10@users.noreply.github.com>
2023-05-11 12:32:47 -0300

    Delete todo directory

--- todo/src/app/menue/menue.component.cy.ts
+++
@@ -1,7 +0,0 @@
-import { MenueComponent } from './menue.component'
-
-describe('MenueComponent', () => {
-  it('should mount', () => {
-    cy.mount(MenueComponent)
-  })
-})
--- todo/src/app/welcome/welcome.component.cy.ts
+++
@@ -1,7 +0,0 @@
-import { WelcomeComponent } from './welcome.component'
-
-describe('WelcomeComponent', () => {
-  it('should mount', () => {
-    cy.mount(WelcomeComponent)
-  })
-})
